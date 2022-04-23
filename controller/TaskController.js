4da3d0e7d80c95e48bb8bfcc490cf96bed6cb45e
joseluiz123 <joseluiz140697@gmail.com>
2022-04-23 07:58:40 -0300

    commit funcionando tudo

---
+++ controller/TaskController.js
@@ -0,0 +1,112 @@
+const Task = require("../models/Task")
+
+let message = ""
+let type = ""
+
+const getAllTasks = async (req, res) => {
+    try {
+        setTimeout(() => {
+            message = ""
+        }, 2000)
+        const tasksList = await Task.find();
+        /* console.log(tasksList) */
+        return res.render("index", {
+            tasksList,
+            task: null,
+            taskDelete: null,
+            message,
+            type
+        })
+
+    } catch (err) {
+        res.status(500).send({ error: err.message });
+    }
+}
+
+const createTask = async (req, res) => {
+    const task = req.body;
+
+    if (!task.task) {
+        message = "Insira uma texto antes de adicionar a tarefa!"
+        type = "danger"
+        return res.redirect("/")
+    }
+
+    try {
+        await Task.create(task)
+        message = "Tarefa criada com sucesso"
+        type = "success"
+        return res.redirect("/")
+    } catch (err) {
+        res.status(500).send({ error: err.message })
+    }
+};
+
+const getByID = async (req, res) => {
+    try {
+        const tasksList = await Task.find();
+
+        if (req.params.method == "update") {
+            const task = await Task.findOne({ _id: req.params.id })
+            res.render("index", { task, tasksList, taskDelete: null, message, type })
+        } else {
+            const taskDelete = await Task.findOne({ _id: req.params.id })
+            //console.log(req.params.id)
+            res.render("index", { task: null, tasksList, taskDelete, message, type })
+        }
+    } catch (err) {
+        res.status(500).send({ error: err.message })
+    }
+}
+
+const updateOneTask = async (req, res) => {
+    try {
+        const task = req.body
+        await Task.updateOne({ _id: req.params.id }, task)
+        message = "Tarefa atualizada com sucesso"
+        type = "success"
+        res.redirect("/")
+    } catch (err) {
+        res.status(500).send({ error: err.message })
+    }
+}
+
+const deleteOneTask = async (req, res) => {
+    const id = req.params.id
+
+    try {
+        await Task.deleteOne({ _id: id })
+        message = "Tarefa apagada com sucesso"
+        type = "success"
+        res.redirect("/")
+    } catch (err) {
+        res.status(500).send({ error: err.message })
+    }
+}
+
+const TaskCheck = async (req, res) => {
+    try {
+        const task = await Task.findOne({ _id: req.params.id })
+
+        if (task.check) {
+            task.check = false
+        } else {
+            task.check = true
+        }
+
+        await Task.updateOne({ _id: req.params.id }, task)
+        res.redirect("/")
+
+    } catch (err) {
+        res.status(500).send({ error: err.message })
+    }
+}
+
+module.exports = {
+    getAllTasks,
+    createTask,
+    getByID,
+    updateOneTask,
+    deleteOneTask,
+    TaskCheck
+}
---
+++ index.js
@@ -0,0 +1,23 @@
+require('dotenv').config()//cria e envia as variáveis de ambiente para poder fazer o deploy para o heroku
+const express = require("express")
+const path = require("path")
+const routes = require("./routes/routes")
+const connectDB = require("./database/db")
+
+connectDB();
+const app = express()
+const port = process.env.PORT
+
+app.set("view engine", "ejs")
+app.use(express.static(path.join(__dirname, "public")))
+app.use(express.urlencoded())
+app.use(routes)
+
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`)
+})
+
+
+
+
+
